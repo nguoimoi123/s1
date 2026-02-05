33b19c7b8cfdf5d0408ad012e840efb4f1d58be8
nguoimoi <buinguyenminhquan84@gmail.com>
2026-02-05 09:22:04 +0700

    sadad

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,7 @@
 from flask_mongoengine import MongoEngine
 from flask_socketio import SocketIO
 
-socketio = SocketIO(cors_allowed_origins="*", async_mode="threading")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
 
 
 db = MongoEngine()
--- run.py
+++ run.py
@@ -1,7 +1,13 @@
+import os
+import eventlet
+
+eventlet.monkey_patch()
+
 from app import create_app
 from app.extensions import socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", "5000"))
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, use_reloader=False)
